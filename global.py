1854c48f31aaa899bf6b2f77156e2e39bc0b3c63
Aamir Meman <aamir.meman@infor.com>
2019-04-05 13:27:57 -0400

    understanding the global keyword

---
+++ global.py
@@ -0,0 +1,16 @@
+"""
+The global keyword will allow the global variable message to be assigned to within the main function without producing
+a new local variable
+"""
+
+message = "This is a global!"
+
+
+def main():
+    global message
+    message = "This is a local"
+    print(message)
+
+
+main()
+print(message)
