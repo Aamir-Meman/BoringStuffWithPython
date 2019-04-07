4f3bcbc7174fedb419561128a7ad9c93a8370c96
Aamir Meman <aamir.meman@infor.com>
2019-04-07 18:30:41 -0400

    adding while loop concept

--- lists.py
+++ lists.py
@@ -1,7 +1,8 @@
 
 # list can have any type in there chain i.e. str, int
 nums = [1, 2, 3, 4, "5", "Aamir"]
-
+print(nums[0])
+print(nums[1])
 nums.append(6)
 
 print(nums)
---
+++ whileloop.py
@@ -0,0 +1,8 @@
+nums = [1, 2, 3, 4, 5]
+counter = 0
+
+while counter < len(nums):
+    print("index: {}, values: {} ".format(counter, nums[counter]))
+    counter = counter + 1
+else:
+    print("sums: {} ".format(sum(nums)))
