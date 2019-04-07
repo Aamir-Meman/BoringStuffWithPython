608395d3f5872e1e6502b2dd9b5c90af5745fe85
Aamir Meman <aamir.meman@infor.com>
2019-04-07 16:37:56 -0400

    adding to understand boolean nature

---
+++ understandingbool.py
@@ -0,0 +1,23 @@
+# one of them is True will com back as True
+print(True or False)
+print(2 or 0)  # one of them is True will return True i.e 2
+
+# one of them is False will come back as False
+print(True and False)
+print(2 and 0)  # one of them is False will return False i.e. 0 ( Falsy value)
+
+# not True will return False
+print(not True)
+
+# not False will return True
+print(not False)
+
+"""
+True
+2
+False
+0
+False
+True
+
+"""
