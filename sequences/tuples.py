e774dab58aa10a44802df6188bff0197ad04aee0
Aamir Meman <aamir.meman@infor.com>
2019-04-08 12:53:05 -0400

    exploring tuples

---
+++ sequences/tuples.py
@@ -0,0 +1,39 @@
+p = ('Bob', 'Smith', 34)
+
+print(p)
+print(type(p))
+print(p[1])
+print(p[2])
+"""
+Unpacking 
+
+"""
+
+fn, ln, age = p
+
+print(fn)
+print(ln)
+print(age)
+
+"""
+Swaping
+
+"""
+
+a = 1
+
+b = 2
+
+b, a = a, b
+
+print(a)
+print(b)
+
+""" 
+Empty Tuple
+
+"""
+
+q = ()
+print(q)
+print(type(q))
