66aee89c298ab6782aa9dd3c18c680cf77b51383
Aamir Meman <aamir.meman@infor.com>
2019-04-10 12:43:35 -0400

    exploring sort and reverse api of python

---
+++ sequences/ordering-list.py
@@ -0,0 +1,11 @@
+
+nums = [2, 5, 7, 8, 3, 2, 1, 6, 7, 4]
+
+# nums.sort()
+
+# Sort in reverse order
+# nums.sort(reverse=True)
+
+# Only in reverse order ( No Sorting is done)
+nums.reverse()
+print(nums)
