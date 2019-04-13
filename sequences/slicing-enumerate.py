2c86eca688e1afac253d0df69ab6849084923248
Aamir Meman <aamir.meman@infor.com>
2019-04-13 00:37:15 -0400

    exploring slicing and enumerate for loops

--- sequences/slicing-enumerate.py
+++ sequences/slicing-enumerate.py
@@ -11,3 +11,14 @@ print(colors[1:3])
 for index, color in enumerate(colors, start=1):
     print(index, color)
 
+"""
+blue
+['blue', 'green', 'yellow']
+['red']
+['blue', 'green']
+1 red
+2 blue
+3 green
+4 yellow
+
+"""
