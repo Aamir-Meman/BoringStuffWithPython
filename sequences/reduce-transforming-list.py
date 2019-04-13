72e9a583020850d1e358b8a7ebe00707314b0b56
Aamir Meman <aamir.meman@infor.com>
2019-04-13 01:12:17 -0400

    exploring the reduce function

--- sequences/reduce-transforming-list.py
+++ sequences/reduce-transforming-list.py
@@ -15,9 +15,13 @@ nums = [1, 2, 3, 4, 5]
 
 def sum_number(total, next_val):
     print("total: {}, next_val: {}".format(total, next_val))
-    return total + next_val
+    return (total + next_val)/2
 
 
-total_sum = reduce(sum_number, nums)
+# total_sum = reduce(sum_number, nums)
+
+total_sum = reduce(lambda acc, current: acc + current, nums)
+
+# total_sum = reduce(lambda x, y: (x + y)/2, nums)
 
 print(total_sum)
