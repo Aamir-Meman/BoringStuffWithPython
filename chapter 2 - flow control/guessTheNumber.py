498ea8ab3c03e66cc264165a95fe31ec56a6719c
Aamir Meman <aamir.meman@infor.com>
2020-10-12 15:44:45 -0400

    just formatting

--- chapter 2 - flow control/guessTheNumber.py
+++ chapter 2 - flow control/guessTheNumber.py
@@ -2,11 +2,11 @@
 import random
 
 # guess the number in between 1 and 20
-secretNumber = random.randint(1,20)
+secretNumber = random.randint(1, 20)
 print('I am thinking number between 1 and 20')
 
 # Guess the number by 6 times
-for guessTaken in range(1,7):
+for guessTaken in range(1, 7):
     print('Take a guess')
     guess = int(input())
 
@@ -17,6 +17,7 @@ for guessTaken in range(1,7):
     else:
         break  # Your guess is correct
 if guess == secretNumber:  # type: ignore
-    print('You guess was correct after ' + str(guessTaken) + ' times.')  # type: ignore
+    print('You guess was correct after ' +
+          str(guessTaken) + ' times.')  # type: ignore
 else:
     print('Nope. The number I was thinking of ' + str(secretNumber))
