2a6285ab395565d0b808ffa32e5a48bc526b8518
Aamir Meman <aamir.meman@infor.com>
2020-10-12 15:46:05 -0400

    proper formatting

--- chapter 2 - flow control/guessTheNumber.py
+++ chapter 2 - flow control/guessTheNumber.py
@@ -16,8 +16,8 @@ for guessTaken in range(1, 7):
         print(' Your guess is too low')
     else:
         break  # Your guess is correct
+
 if guess == secretNumber:  # type: ignore
-    print('You guess was correct after ' +
-          str(guessTaken) + ' times.')  # type: ignore
+    print('You guess was correct after ' + str(guessTaken) + ' times.')  # type: ignore
 else:
     print('Nope. The number I was thinking of ' + str(secretNumber))
